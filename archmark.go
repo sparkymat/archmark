d21ce6468e241fb6da03baa372731cfbc7636607
Ajith Hussain <ajith.hussain@fastmail.com>
2021-09-30 21:20:58 +1000

    Accepting URL

--- archmark.go
+++ archmark.go
@@ -6,11 +6,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type AddInput struct {
+	Url string `json:"url" binding:"required"`
+}
+
 func main() {
 	r := gin.Default()
 	r.POST("/add", func(c *gin.Context) {
+		var input AddInput
+
+		if c.Bind(&input) != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "unimplemented",
+			})
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"message": "unimplemented",
+			"message": "got url",
 		})
 	})
 	r.Run(":8080")
