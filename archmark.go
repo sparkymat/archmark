ef4cd48491fc9546d2876542d12c4c1d8c76549d
Ajith Hussain Subayar <ajith.hussain@fastmail.com>
2021-10-04 00:18:48 +1100

    Archiving logic!

--- archmark.go
+++ archmark.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -12,6 +11,7 @@ import (
 	"github.com/chromedp/cdproto/input"
 	"github.com/chromedp/cdproto/runtime"
 	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/kb"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/sparkymat/archmark/config"
@@ -21,7 +21,7 @@ type AddInput struct {
 	Url string `json:"url" binding:"required"`
 }
 
-func retrieve(cfg config.API, url string) string {
+func retrieve(cfg config.API, url string) (string, string, error) {
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("ignore-certificate-errors", "1"),
 	)
@@ -33,7 +33,7 @@ func retrieve(cfg config.API, url string) string {
 	ctx, cancel = chromedp.NewContext(ctx, chromedp.WithLogf(log.Printf))
 	defer cancel()
 
-	var buf []byte
+	var title, fileUrl string
 	if err := chromedp.Run(ctx,
 		chromedp.Navigate(fmt.Sprintf("%s/admin/login/", cfg.ArchiveBoxPath())),
 		chromedp.WaitVisible("#id_username", chromedp.ByQuery),
@@ -53,21 +53,18 @@ func retrieve(cfg config.API, url string) string {
 		}, chromedp.ByQuery),
 		chromedp.Click("#submit", chromedp.ByQuery),
 		chromedp.WaitVisible("pre#stdout", chromedp.ByQuery),
-		chromedp.FullScreenshot(&buf, 90),
+		chromedp.Navigate(fmt.Sprintf("%s/public/", cfg.ArchiveBoxPath())),
+		chromedp.WaitVisible("#searchbar", chromedp.ByQuery),
+		chromedp.SendKeys("#searchbar", url, chromedp.ByQuery),
+		chromedp.SendKeys("#searchbar", kb.Enter, chromedp.ByQuery),
+		chromedp.WaitVisible("#searchbar", chromedp.ByQuery),
+		chromedp.Text(".title-col > a:nth-child(2) > span:nth-child(1)", &title, chromedp.ByQuery),
+		chromedp.AttributeValue("a[title='singlefile']", "href", &fileUrl, nil, chromedp.ByQuery),
 	); err != nil {
-		if err := ioutil.WriteFile("fullScreenshot.png", buf, 0644); err != nil {
-			log.Print(err)
-			return ""
-		}
-		log.Print(err)
-		return ""
-	}
-	if err := ioutil.WriteFile("fullScreenshot.png", buf, 0644); err != nil {
-		log.Print(err)
-		return ""
+		return "", "", err
 	}
 
-	return ""
+	return title, fmt.Sprintf("%s%s", cfg.ArchiveBoxPath(), fileUrl), nil
 }
 
 const ConfigKey = "config"
@@ -109,11 +106,18 @@ func main() {
 			return
 		}
 
-		log.Print(retrieve(cfg, input.Url))
+		title, url, err := retrieve(cfg, input.Url)
 
-		c.JSON(http.StatusOK, gin.H{
-			"message": "got url",
-		})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"title": title,
+				"url":   url,
+			})
+		}
 	})
 	r.Run(":8080")
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/sparkymat/archmark
 go 1.17
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20211002082225-0242b9dca9f4 // indirect
 	github.com/chromedp/chromedp v0.7.4
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.7.4
@@ -26,5 +25,15 @@ require (
 
 require (
 	github.com/caarlos0/env/v6 v6.7.1
+	github.com/chromedp/cdproto v0.0.0-20211002082225-0242b9dca9f4
 	github.com/joho/godotenv v1.4.0
+)
+
+require (
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 )
