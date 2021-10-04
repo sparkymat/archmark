6fc71d45f78f99db1d91b76d84ca50a944ab9549
Ajith Hussain Subayar <ajith.hussain@fastmail.com>
2021-10-04 22:40:58 +1100

    Moved archiving logic to service

--- archmark.go
+++ archmark.go
@@ -1,70 +1,17 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
-	"time"
 
-	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/cdproto/input"
-	"github.com/chromedp/cdproto/runtime"
-	"github.com/chromedp/chromedp"
-	"github.com/chromedp/chromedp/kb"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/sparkymat/archmark/archivebox"
 	"github.com/sparkymat/archmark/config"
 )
 
 type AddInput struct {
 	Url string `json:"url" binding:"required"`
-}
-
-func retrieve(cfg config.API, url string) (string, string, error) {
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("ignore-certificate-errors", "1"),
-	)
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
-
-	ctx, cancel := context.WithTimeout(allocCtx, time.Second*30)
-
-	ctx, cancel = chromedp.NewContext(ctx, chromedp.WithLogf(log.Printf))
-	defer cancel()
-
-	var title, fileUrl string
-	if err := chromedp.Run(ctx,
-		chromedp.Navigate(fmt.Sprintf("%s/admin/login/", cfg.ArchiveBoxPath())),
-		chromedp.WaitVisible("#id_username", chromedp.ByQuery),
-		chromedp.SendKeys("#id_username", cfg.ArchiveBoxUsername(), chromedp.ByQuery),
-		chromedp.SendKeys("#id_password", cfg.ArchiveBoxPassword(), chromedp.ByQuery),
-		chromedp.Submit("#id_username", chromedp.ByQuery),
-		chromedp.WaitVisible("#recent-actions-module", chromedp.ByQuery),
-		chromedp.Navigate(fmt.Sprintf("%s/add/", cfg.ArchiveBoxPath())),
-		chromedp.WaitVisible("#id_url", chromedp.ByQuery),
-		chromedp.SendKeys("#id_url", url, chromedp.ByQuery),
-		chromedp.Click("#id_depth_0", chromedp.ByQuery),
-		chromedp.QueryAfter("#id_archive_methods > option[value=\"title\"]", func(ctx context.Context, id runtime.ExecutionContextID, nodes ...*cdp.Node) error {
-			return chromedp.MouseClickNode(nodes[0], chromedp.ButtonModifiers(input.ModifierCtrl)).Do(ctx)
-		}, chromedp.ByQuery),
-		chromedp.QueryAfter("#id_archive_methods > option[value=\"singlefile\"]", func(ctx context.Context, id runtime.ExecutionContextID, nodes ...*cdp.Node) error {
-			return chromedp.MouseClickNode(nodes[0], chromedp.ButtonModifiers(input.ModifierCtrl)).Do(ctx)
-		}, chromedp.ByQuery),
-		chromedp.Click("#submit", chromedp.ByQuery),
-		chromedp.WaitVisible("pre#stdout", chromedp.ByQuery),
-		chromedp.Navigate(fmt.Sprintf("%s/public/", cfg.ArchiveBoxPath())),
-		chromedp.WaitVisible("#searchbar", chromedp.ByQuery),
-		chromedp.SendKeys("#searchbar", url, chromedp.ByQuery),
-		chromedp.SendKeys("#searchbar", kb.Enter, chromedp.ByQuery),
-		chromedp.WaitVisible("#searchbar", chromedp.ByQuery),
-		chromedp.Text(".title-col > a:nth-child(2) > span:nth-child(1)", &title, chromedp.ByQuery),
-		chromedp.AttributeValue("a[title='singlefile']", "href", &fileUrl, nil, chromedp.ByQuery),
-	); err != nil {
-		return "", "", err
-	}
-
-	return title, fmt.Sprintf("%s%s", cfg.ArchiveBoxPath(), fileUrl), nil
 }
 
 const ConfigKey = "config"
@@ -83,7 +30,6 @@ func main() {
 	}
 
 	cfg := config.New()
-	log.Printf("%+v", cfg)
 
 	r := gin.Default()
 	r.Use(ConfigMiddleware(cfg))
@@ -95,7 +41,14 @@ func main() {
 			})
 			return
 		}
+
 		cfg := cfgVal.(config.API)
+
+		archiveBoxAPI := archivebox.New(archivebox.Config{
+			Path:     cfg.ArchiveBoxPath(),
+			Username: cfg.ArchiveBoxUsername(),
+			Password: cfg.ArchiveBoxPassword(),
+		})
 
 		var input AddInput
 
@@ -106,7 +59,7 @@ func main() {
 			return
 		}
 
-		title, url, err := retrieve(cfg, input.Url)
+		title, url, err := archiveBoxAPI.ArchiveLink(input.Url)
 
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
