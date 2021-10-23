58e9885bed1c77f69ad252930a01f6021f96f1fd
Ajith Hussain <ajith.hussain@fastmail.com>
2021-10-23 16:24:01 +1100

    WIP: Find bookmark

--- database/bookmark.go
+++ database/bookmark.go
@@ -57,27 +57,47 @@ func (s *service) ListBookmarks(ctx context.Context, query string, page uint64, 
 	}
 
 	return bookmarks, nil
-	/*
-		var bookmarks []model.Bookmark
-
-		offset := int((page - 1) * pageSize)
-		stmnt := s.conn
-
-		if query != "" {
-			stmnt = stmnt.Where("to_tsvector(content) @@ to_tsquery(?)", query)
-		} else {
-			stmnt = stmnt.Order("created_at desc")
-		}
-
-		if result := stmnt.Offset(offset).Limit(int(pageSize)).Find(&bookmarks); result.Error != nil {
-			return nil, result.Error
-		}
-
-		return bookmarks, nil
-	*/
 }
 
 func (s *service) FindBookmark(ctx context.Context, id uint64) (*model.Bookmark, error) {
+	psql := sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
+
+	stmnt := psql.
+		Select("*").
+		From("bookmarks").
+		Where("id = ?", id)
+
+	querySQL, args, err := stmnt.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate sql. err: %w", err)
+	}
+
+	log.Printf("SQL: %s\n", querySQL)
+
+	var bookmarks model.Bookmark
+
+	rows, err := s.conn.QueryxContext(ctx, querySQL, args...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to run query. err: %w", err)
+	}
+
+	for rows.Next() {
+		var bookmark model.Bookmark
+
+		var deletedAt sql.NullTime
+		err := rows.StructScan(&bookmark)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan row. err: %w", err)
+		}
+
+		if deletedAt.Valid {
+			bookmark.DeletedAt = &deletedAt.Time
+		}
+
+		bookmarks = append(bookmarks, bookmark)
+	}
+
+	return bookmarks, nil
 	panic("unimplemented")
 	/*
 		bookmark := &model.Bookmark{}
