a9a67983a29186c6aaeb74c76a6fedb6220659c9
Ajith Hussain <ajith.hussain@fastmail.com>
2021-10-25 19:55:04 +1100

    Setting User Agent string

--- archive/service.go
+++ archive/service.go
@@ -13,7 +13,12 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-var ErrUnableToCreateFile = errors.New("unable to create file")
+var (
+	ErrUnableToCreateFile = errors.New("unable to create file")
+	ErrURLFetchFailed     = errors.New("url fetch failed")
+)
+
+const okStatusPrefix = 2
 
 type Config struct {
 	DownloadFolder string
@@ -51,6 +56,9 @@ func (s *service) Save(ctx context.Context, url string, fileName string) (*Archi
 		return nil, fmt.Errorf("failed to create http request. err: %w", err)
 	}
 
+	//nolint:lll
+	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch url. err: %w", err)
@@ -64,6 +72,11 @@ func (s *service) Save(ctx context.Context, url string, fileName string) (*Archi
 	}
 
 	body := string(bodyBytes)
+
+	// Check if it is a 2xx status code
+	if resp.StatusCode/100 != okStatusPrefix {
+		return nil, ErrURLFetchFailed
+	}
 
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(body))
 	if err != nil {
